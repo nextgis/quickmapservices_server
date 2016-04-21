fa7bc200e2abb0a8d2eec3ed9e3c78ed139ef91a
yellow-sky <nikulin.e@gmail.com>
2016-04-21 13:54:28 +0300

    Fix url names

--- qms_server/qms_site/urls.py
+++ qms_server/qms_site/urls.py
@@ -3,6 +3,6 @@ from qms_site.views import GeoserviceListView, GeoserviceDetailView
 
 
 urlpatterns = [
-    url(r'^$', GeoserviceListView.as_view(), name='geoservice_list'),
-    url(r'^geoservices/(?P<pk>[0-9]+)/$', GeoserviceDetailView.as_view(), name='geoservice_detail'),
+    url(r'^$', GeoserviceListView.as_view(), name='site_geoservice_list'),
+    url(r'^geoservices/(?P<pk>[0-9]+)/$', GeoserviceDetailView.as_view(), name='site_geoservice_detail'),
 ]
