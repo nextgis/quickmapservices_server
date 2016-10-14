eb221daf484fc270e7b0e71fa0d5651b1b763586
yellow-sky <nikulin.e@gmail.com>
2016-10-14 19:23:37 +0300

    Add ru translations

--- qms_server/qms_server/settings.py
+++ qms_server/qms_server/settings.py
@@ -55,13 +55,13 @@ MIDDLEWARE_CLASSES = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'qms_stats.middleware.LoggingMiddleware',
 ]
 
 TEMPLATES = [
