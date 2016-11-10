a24e86cea01887cd4938b2608acd31c0856dbfe0
yellow-sky <nikulin.e@gmail.com>
2016-11-10 18:14:20 +0300

    Add form type for templates

--- qms_server/qms_site/views.py
+++ qms_server/qms_site/views.py
@@ -1,8 +1,10 @@
+from django.http import JsonResponse
+from django.views.defaults import bad_request
 from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
 from qms_core.models import GeoService
 from qms_site.forms import TmsForm, WmsForm, WfsForm, GeoJsonForm
-
+from django.utils.translation import gettext_lazy as _
 
 class GeoserviceListView(TemplateView):
     template_name = 'list.html'
@@ -34,12 +36,46 @@ class CreateServiceView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(CreateServiceView, self).get_context_data(**kwargs)
         forms = {
-            'tms_form': TmsForm(),
-            'wms_form': WmsForm(),
-            'wfs_form': WfsForm(),
-            'geojson_form': GeoJsonForm()
+            TmsForm.__name__: TmsForm(),
+            WmsForm.__name__: WmsForm(),
+            WfsForm.__name__: WfsForm(),
+            GeoJsonForm.__name__: GeoJsonForm()
         }
 
         context['forms'] = forms
 
         return context
+
+    def post(self, request, *args, **kwargs):
+
+        form_class_name = request.POST.get('service_type', None)
+
+        if not form_class_name:
+            return bad_request(request, _('Invalid form param: service_type'))
+
+        if form_class_name not in (TmsForm.__name__, WmsForm.__name__, WfsForm.__name__, GeoJsonForm.__name__):
+            return bad_request(request, _('Invalid form param: service_type'))
+
+        form_class = globals()[form_class_name]
+        form = form_class(**self.get_form_kwargs())
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+
+    def get_form_kwargs(self):
+        kwargs = {}
+        if self.request.method in ('POST', 'PUT'):
+            kwargs.update({
+                'data': self.request.POST,
+                'files': self.request.FILES,
+            })
+        return kwargs
+
+    def form_valid(self, form):
+        return JsonResponse({'status': 'valid'})
+
+    def form_invalid(self, form):
+        return JsonResponse({'status': 'invalid', 'errors': form.errors})
