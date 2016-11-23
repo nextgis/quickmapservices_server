ae45ad22f6571d84f1ac7f73d174fbdf115f4e09
lisovenkoNastya <lisovenko.ad@gmail.com>
2016-11-23 13:51:16 +0300

    pass all license form fields in add service form

--- qms_server/qms_site/static/qms_site/js/qms.js
+++ qms_server/qms_site/static/qms_site/js/qms.js
@@ -2,16 +2,27 @@
 var QMSAddForm = (function(){
     var qmsAddForm = $(".qms-add-service");
 
+    function isFormFilled(form){
+        var result = false;
+        form.find("input, select").each(function(){
+            if ($(this).val()) {
+                result = true;
+                return false;
+            }
+        });
+        return result;
+    }
+
     var me = {
         init: function(){
             qmsAddForm.each(function(){
                 var form = $(this),
                     licenseForm = form.find(".qms-add-service__license-form"),
-                    licenseNameControl = licenseForm.find(".qms-add-service__license-name"),
                     licenseInfo = form.find(".qms-add-service__license-info");
 
                 licenseForm.on("innerForm.save", function(){
-                    if (licenseNameControl.val()){
+                    console.log("innerForm.save")
+                    if (isFormFilled(licenseForm)){
                         licenseInfo.addClass("filled");
                     } else{
                         licenseInfo.removeClass("filled");
