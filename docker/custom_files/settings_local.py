777e1d194c618d8974a64eaa40de3fc78ca349c4
Sergey Kazarin <mechmskv@yandex.ru>
2021-02-16 17:44:34 +0300

    docker

---
+++ docker/custom_files/settings_local.py
@@ -0,0 +1,60 @@
+"""
+Django settings for qms_site project.
+"""
+
+# Override
+DEBUG = True
+ALLOWED_HOSTS = ['qms.nextgis.com', '127.0.0.1']
+
+DATABASE_NAME = os.getenv('DATABASE_NAME','')
+DATABASE_USER = os.getenv('DATABASE_USER','')
+DATABASE_PASS = os.getenv('DATABASE_PASS','')
+DATABASE_HOST = os.getenv('DATABASE_HOST','')
+DATABASE_PORT = os.getenv('DATABASE_PORT','')
+
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DATABASE_NAME,
+        'USER': DATABASE_USER,	                # SET THIS!
+        'PASSWORD': DATABASE_PASS,             # SET THIS!
+        'HOST': DATABASE_HOST,                 # SET THIS!
+        'PORT': DATABASE_PORT
+    }
+}
+
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+# Secure
+SECRET_KEY = 'ASDFLKAL;SKDF;LKN3M,N2LKJ34H5KJF;GHSUI898993243(N	KLS-FQ9'
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
+# NGID Auth
+NGID_CLIENT_ID = 'jQBKtVVaEWTIZT9yZQt9pet6168iTD5sR27dYXT8'
+NGID_CLIENT_SECRET = 'AlWUFBZXWT0a7wzuFt1rvajgVDuyQXwIOvEWPRVjAJm1Wcx9b0vPTYCPgPfaaqpEGGmZNK9MKNQpfl9n1V2VUwaAG8XrFAybxpCDz1r8m5wvfnQZZdxBZ3aHhmZkVw77'
+
+
+
+# reCaptcha settings
+RECAPTCHA_PUBLIC_KEY = '6Lcu7xUUAAAAAJ-fUPBp50JiHqkSJHeqLCf0uFxp'
+RECAPTCHA_PRIVATE_KEY = '6Lcu7xUUAAAAALwAK5Mlc6ioggDVvjccO4HeFR2j'
+NOCAPTCHA = True
+
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ''                     # SET THIS!
+EMAIL_PORT = 587                    # SET THIS!
+EMAIL_HOST_USER = ''                # SET THIS!
+EMAIL_HOST_PASSWORD = ''            # SET THIS!
+EMAIL_USE_TLS = True                # SET THIS!
+EMAIL_USE_SSL = False               # SET THIS!
+DEFAULT_FROM_EMAIL = ''             # SET THIS!
+
+# Checker settings
+SERVICE_CHECK_TIMEOUT = 10  # secs
---
+++ docker/get_code.sh
@@ -0,0 +1,9 @@
+mkdir ../../qms.docker
+pushd ../../qms.docker
+git clone --recurse-submodules https://github.com/nextgis/quickmapservices_server.git
+cd quickmapservices_server
+git checkout py3dj22
+popd
+cp ./custom_files/settings_local.py ../../qms.docker/quickmapservices_server/qms_server/qms_server/settings_local.py
+cp ./custom_files/views.py ../../qms.docker/quickmapservices_server/qms_server/nextgis_common/ngid_auth/views.py
+
