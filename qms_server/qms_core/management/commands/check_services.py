d93b248109bfdbaf58552345434c029d16765d5f
alisovenko <alexander.lisovenko@gmail.com>
2021-03-18 13:59:49 +0300

    Improve check_services, down pyproj version

--- qms_server/qms_core/management/commands/check_services.py
+++ qms_server/qms_core/management/commands/check_services.py
@@ -3,7 +3,13 @@ import pprint
 
 from django.core.management import BaseCommand
 
-from qms_core.models import GeoService
+from qms_core.models import (
+    GeoService,
+    TmsService,
+    WmsService,
+    WfsService,
+    GeoJsonService,
+)
 from qms_core.status_checker.status_checker import check_by_id, check_by_id_and_save
 
 
@@ -12,7 +18,10 @@ class Command(BaseCommand):
     output_transaction = True
 
     def add_arguments(self, parser):
+        service_type_choices = [TmsService.service_type, WmsService.service_type, WfsService.service_type, GeoJsonService.service_type]
+
         parser.add_argument('-t', '--threads', help='Threads count for checkers', type=int, default=20)
+        parser.add_argument('--service_type', choices=service_type_choices, help='Service type')
         parser.add_argument('--service', help='Internal service id', type=int)
 
 
@@ -20,10 +29,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         thread_count = options['threads']
 
+        service_ids = GeoService.objects.all().only('id').values_list(flat=True)
+        
         if options.get('service'):
-            service_ids = [options['service']]
-        else:
-            service_ids = GeoService.objects.all().only('id').values_list(flat=True)
+            service_ids = service_ids.filter(id=options['service'])
+        
+        if options.get('service_type'):
+            service_ids = service_ids.filter(type=options['service_type'])
+
+        print('Try to check %s services' % len(service_ids))
 
         checkers_pool = multiprocessing.Pool(thread_count)
         checkers_pool.imap(check_by_id_and_save, service_ids)
